1d56d6f5cef5ba6122e01b295b36ac2c5bb311d3
zubeyr <zubeyrdamar@outlook.com>
2023-12-11 05:34:39 +0300

    Database Operations

---
+++ Walks/Walks.API/Data/WalksDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Walks.API.Models;
+
+namespace Walks.API.Data
+{
+    public class WalksDbContext : DbContext
+    {
+        public WalksDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
