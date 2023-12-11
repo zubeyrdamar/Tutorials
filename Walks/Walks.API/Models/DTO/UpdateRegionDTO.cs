ec7f3c30a344bdb997bfd0f5d445f54f7c76d715
zubeyr <zubeyrdamar@outlook.com>
2023-12-11 06:18:26 +0300

    Regions Controller Methods

---
+++ Walks/Walks.API/Models/DTO/UpdateRegionDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Walks.API.Models.DTO
+{
+    public class UpdateRegionDTO
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
