be82a273cc1dd37b7fd7221434c671624a1efc01
zubeyr <zubeyrdamar@outlook.com>
2023-12-11 21:55:56 +0300

    Validation Example (Create Region)

--- Walks/Walks.API/Models/DTO/CreateRegionDTO.cs
+++ Walks/Walks.API/Models/DTO/CreateRegionDTO.cs
@@ -1,9 +1,16 @@
-﻿namespace Walks.API.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Walks.API.Models.DTO
 {
     public class CreateRegionDTO
     {
+        [Required]
+        [MinLength(2, ErrorMessage = "Code must contain 2 digits")]
+        [MaxLength(2, ErrorMessage = "Code must contain 2 digits")]
         public string Code { get; set; }
+
+        [Required]
         public string Name { get; set; }
-        public string? ImageUrl { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
